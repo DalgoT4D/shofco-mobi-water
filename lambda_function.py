d12c6579fcf3d5ddf551b251d89629a819a4b28d
Rohit K. Chatterjee <fatchat@users.noreply.github.com>
2025-01-04 23:40:34 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,10 @@ import json
 import urllib3
 from urllib.parse import urlparse, parse_qs
 from datetime import datetime, timedelta
+import dateutil.tz
+
+def validate_timezone(tz_name):
+    return dateutil.tz.gettz(tz_name)
 
 def lambda_handler(event, context):
     http = urllib3.PoolManager()
@@ -35,22 +39,63 @@ def lambda_handler(event, context):
                     "body": "missing parameter flow_device_id"
                 }
 
+            if "startdate" not in url_parameters:
+                return {
+                    "statusCode": 400,
+                    "body": "missing parameter startdate"
+                }
+
+            if "tz" not in url_parameters:
+                return {
+                    "statusCode": 400,
+                    "body": "missing parameter tz"
+                }
+
             flow_device_id = url_parameters["flow_device_id"][0]
+            startdate_str = url_parameters["startdate"][0]
+            iana_tz = url_parameters["tz"][0]
 
-            if "fromDate" in url_parameters:
-                from_date = url_parameters["fromDate"][0]
-            else:
-                from_date = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+            # print(flow_device_id)
+            # print(startdate_str)
+            # print(iana_tz)
 
-            if "toDate" in url_parameters:
-                to_date = url_parameters['toDate'][0]
-            else:
-                to_date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-                
-            url = base_url + f"monitoring/v1/flowdevices/flowDeviceAnalytics/consumption/{flow_device_id}?toDate={to_date}&fromDate={from_date}"
-            print(url)
-            r = http.request('GET', url, headers=headers)
+            tz = validate_timezone(iana_tz)
+            if tz is None:
+                return {
+                    "statusCode": 400,
+                    "body": "invalid timezone parameter " + iana_tz
+                }
+
+
+            retval = []
+            startdate = datetime.strptime(startdate_str, "%Y-%m-%d").replace(tzinfo=tz)
+            enddate = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=tz)
+
+            dtiter = startdate
+            while dtiter < enddate:
+                q_sdate = dtiter.strftime("%Y-%m-%d %H:%M:%S")
+                q_edate = (dtiter + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")                
+                url = base_url + f"monitoring/v1/flowdevices/flowDeviceAnalytics/consumption/{flow_device_id}?toDate={q_edate}&fromDate={q_sdate}"
+                # print(url)
+                r = http.request('GET', url, headers=headers)
+                retval.append({
+                    "flow_device_id": flow_device_id,
+                    "date": dtiter.strftime("%Y-%m-%d"),
+                    "value": r.data.decode('utf8')
+                })
+                dtiter += timedelta(days=1)
+                # print(dtiter.strftime("%Y-%m-%d") + "    " + enddate.strftime("%Y-%m-%d"))
             return {
                 'statusCode': 200,
-                'body': {"value": r.data.decode('utf8'), "flow_device_id": flow_device_id}
+                'body': retval
             }
+        else:
+            return {
+                "statusCode": 400,
+                "body": "missing query parameters"
+            }
+    else:
+        return {
+            "statusCode": 400,
+            "body": "unknown endpoint " + event['rawPath']
+        }
